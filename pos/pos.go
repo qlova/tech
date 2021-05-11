549ed23de80d904ee6b1525722bfbb4d6da41863
Quentin Quaadgras <email@quentinquaadgras.com>
2021-05-11 19:42:38 +1200

    pos: add WithScales.

--- pos/pos.go
+++ pos/pos.go
@@ -14,7 +14,16 @@ func At(x, y float32) *gpu.Transform {
 //and uniformly scaled with the given scale.
 func WithScale(x, y float32, scale float32) *gpu.Transform {
 	var t = gpu.NewTransform()
-	t.SetScale(scale, scale, scale)
+	t.SetScale(scale, scale, 1)
+	t.SetPosition(x, y, 0)
+	return &t
+}
+
+//WithScales returns a transform to the given (x, y) position
+//and scaled in the x and y direction.
+func WithScales(x, y float32, sx, sy float32) *gpu.Transform {
+	var t = gpu.NewTransform()
+	t.SetScale(sx, sy, 1)
 	t.SetPosition(x, y, 0)
 	return &t
 }
